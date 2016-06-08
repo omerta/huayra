ff3c812f01b088ad55b90bea76f2de463eefd7b7
Lengemann, Guillermo <glengemann@opsu.gob.ve>
2016-06-08 15:59:48 -0400

    [SAF] Se crea SQL para crear la tabla *saf_sudebip_modelo_bienes_muebles*

---
+++ doc/saf/saf_sudebip_modelo_bienes_muebles.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE saf_sudebip_modelo_bienes_muebles (
+	codemp VARCHAR(4),
+	id_modelo serial, -- Códígo interno del modelo asigando por el órgano o ente
+	denominacion_modelo VARCHAR(100), -- Denominación del modelo según el fabricante
+	id_marca integer references saf_sudebip_marcas_bienes_muebles(id_marca), -- Código interno de la marca asignado por el órgano o ente
+	codigo_especifico varchar references saf_catalogo_epecifica(codigo_especifico), -- Categoría específica del bien mueble (tercer nivel), distinto de "Semovientes"
+	PRIMARY KEY (id_modelo) -- Llave primaria
+);
