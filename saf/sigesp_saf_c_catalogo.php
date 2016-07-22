c217270f8fc3cabfba8b40a92dac7664a58329ab
Lengemann, Guillermo <glengemann@opsu.gob.ve>
2016-07-22 14:20:48 -0400

    [SAF] Se reescribe el formulario 'Definición de Catálogo'.

--- saf/sigesp_saf_c_catalogo.php
+++ saf/sigesp_saf_c_catalogo.php
@@ -2,7 +2,8 @@
 require_once("../shared/class_folder/class_sql.php");
 require_once("../shared/class_folder/class_datastore.php");
 require_once("../shared/class_folder/class_mensajes.php");
-require_once("../shared/class_folder/sigesp_c_seguridad.php");
+//require_once("../shared/class_folder/sigesp_c_seguridad.php");
+require_once("../shared/class_folder/sigesp_c_seguridad_26042016.php");
 require_once("../shared/class_folder/sigesp_include.php");
 require_once("../shared/class_folder/class_funciones.php");
 
@@ -21,20 +22,20 @@ class sigesp_saf_c_catalogo
 		$this->io_sql=new class_sql($this->con);
 		$this->seguridad= new sigesp_c_seguridad();
 		$this->io_funcion = new class_funciones();
-        $this->ls_codemp=$_SESSION["la_empresa"]["codemp"];
+    $this->ls_codemp=$_SESSION["la_empresa"]["codemp"];
 		$this->msg=new class_mensajes();
 	}
-	
+
 	function uf_saf_select_catalogo($as_catalogo)
 	{
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		//	     Function: uf_saf_select_catalogo
-		//         Access: public 
+		//         Access: public
 		//      Argumento: $as_catalogo    // codigo de catalogo sigecof
 		//	      Returns: Retorna un Booleano
 		//    Description: Esta funcion busca una codificacion del catalogo SIGECOF en la tabla de  saf_catalogo
 		//	   Creado Por: Ing. Luis Anibal Lang
-		// Fecha Creación: 01/01/2006 								Fecha Última Modificación :  
+		// Fecha Creaciï¿½n: 01/01/2006 								Fecha ï¿½ltima Modificaciï¿½n :
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		$lb_valido=true;
 		$ls_sql = "SELECT * FROM saf_catalogo  ".
@@ -42,7 +43,7 @@ class sigesp_saf_c_catalogo
 		$rs_data=$this->io_sql->select($ls_sql);
 		if($rs_data===false)
 		{
-			$this->io_msg->message("CLASE->catalogo MÉTODO->uf_saf_select_catalogo ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
+			$this->io_msg->message("CLASE->catalogo Mï¿½TODO->uf_saf_select_catalogo ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
 			$lb_valido=false;
 		}
 		else
@@ -50,76 +51,79 @@ class sigesp_saf_c_catalogo
 			if($row=$this->io_sql->fetch_row($rs_data))
 			{
 				$lb_valido=true;
-				
+
 			}
 			else
 			{
 				$lb_valido=false;
 			}
 		}
-			
+
 		$this->io_sql->free_result($rs_data);
 		return $lb_valido;
 
-	}//fin de la function uf_saf_select_catalogo 
+	}//fin de la function uf_saf_select_catalogo
 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//	     Function: uf_saf_insert_catalogo
+	//         Access: public
+	//      Argumento: $as_catalogo     // codigo de catalogo sigecof
+	//                 $as_denominacion // denominacion del catalogo
+	//                 $as_cuenta       // cuenta presupuestaria asociada
+	//                 $aa_seguridad    // arreglo de registro de seguridad
+	//	      Returns: Retorna un Booleano
+	//    Description: Esta funcion inserta un nuevo registro al catalogo SIGECOF
+	//	   Creado Por: Ing. Luis Anibal Lang
+	// Fecha Creaciï¿½n: 01/01/2006 								Fecha ï¿½ltima Modificaciï¿½n :
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	function  uf_saf_insert_catalogo($as_catalogo,$as_denominacion,$as_cuenta,$aa_seguridad)
 	{
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		//	     Function: uf_saf_insert_catalogo
-		//         Access: public 
-		//      Argumento: $as_catalogo     // codigo de catalogo sigecof
-		//                 $as_denominacion // denominacion del catalogo
-		//                 $as_cuenta       // cuenta presupuestaria asociada
-		//                 $aa_seguridad    // arreglo de registro de seguridad
-		//	      Returns: Retorna un Booleano
-		//    Description: Esta funcion inserta un nuevo registro al catalogo SIGECOF
-		//	   Creado Por: Ing. Luis Anibal Lang
-		// Fecha Creación: 01/01/2006 								Fecha Última Modificación :  
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		$lb_valido=true;
 		$this->io_sql->begin_transaction();
 		$ls_sql = "INSERT INTO saf_catalogo (catalogo, dencat, spg_cuenta) ".
-				  " VALUES ( '".$as_catalogo."','".$as_denominacion."','".$as_cuenta."' )" ;
+				  		" VALUES ( '".$as_catalogo."','".$as_denominacion."','".$as_cuenta."' )" ;
 		$li_row=$this->io_sql->execute($ls_sql);
 		if($li_row===false)
 		{
-			$this->io_msg->message("CLASE->catalogo MÉTODO->uf_saf_insert_catalogo ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
-			$lb_valido=false;
+			$lb_valido[0]=false;
+			$lb_valido[1]=$this->io_msg->message;
 			$this->io_sql->rollback();
-
 		}
 		else
 		{
-				$lb_valido=true;
-				/////////////////////////////////         SEGURIDAD               /////////////////////////////		
+				$this->io_sql->commit();
+				$lb_valido[0]=true;
+				/////////////////////////////////         SEGURIDAD               /////////////////////////////
 				$ls_evento="INSERT";
-				$ls_descripcion ="Insertó en el Catalogo SIGECOF ".$as_catalogo;
+				$ls_descripcion ="Insert&oacute; en el Catalogo SIGECOF ".$as_catalogo;
 				$ls_variable= $this->seguridad->uf_sss_insert_eventos_ventana($aa_seguridad["empresa"],
 												$aa_seguridad["sistema"],$ls_evento,$aa_seguridad["logusr"],
 												$aa_seguridad["ventanas"],$ls_descripcion);
-				/////////////////////////////////         SEGURIDAD               /////////////////////////////		
-				$this->io_sql->commit();
+				if($ls_variable[0] == false)
+				{
+					$lb_valido[1]=$ls_variable[1];
+				}else{
+					$lb_valido[1]="";
+				}
+				/////////////////////////////////         SEGURIDAD               /////////////////////////////
 		}
-		
-		return $lb_valido;
 
+		return json_encode($lb_valido);
 	}//fin de la uf_saf_insert_catalogo
 
-	function uf_saf_update_catalogo($as_catalogo,$as_denominacion,$as_cuenta,$aa_seguridad) 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//	     Function: uf_saf_update_catalogo
+	//         Access: public
+	//      Argumento: $as_catalogo     // codigo de catalogo sigecof
+	//                 $as_denominacion // denominacion del catalogo
+	//                 $as_cuenta       // cuenta presupuestaria asociada
+	//                 $aa_seguridad    // arreglo de registro de seguridad
+	//	      Returns: Retorna un Booleano
+	//    Description: Esta funcion actualiza un registro al catalogo SIGECOF
+	//	   Creado Por: Ing. Luis Anibal Lang
+	// Fecha Creaciï¿½n: 01/01/2006 								Fecha ï¿½ltima Modificaciï¿½n :
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	function uf_saf_update_catalogo($as_catalogo,$as_denominacion,$as_cuenta,$aa_seguridad)
 	{
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		//	     Function: uf_saf_update_catalogo
-		//         Access: public 
-		//      Argumento: $as_catalogo     // codigo de catalogo sigecof
-		//                 $as_denominacion // denominacion del catalogo
-		//                 $as_cuenta       // cuenta presupuestaria asociada
-		//                 $aa_seguridad    // arreglo de registro de seguridad
-		//	      Returns: Retorna un Booleano
-		//    Description: Esta funcion actualiza un registro al catalogo SIGECOF
-		//	   Creado Por: Ing. Luis Anibal Lang
-		// Fecha Creación: 01/01/2006 								Fecha Última Modificación :  
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		 $lb_valido=false;
 		 $ls_sql = "UPDATE saf_catalogo SET   dencat='". $as_denominacion ."', spg_cuenta='". $as_cuenta ."' ".
 				   " WHERE catalogo='".$as_catalogo."' ";
@@ -127,38 +131,43 @@ class sigesp_saf_c_catalogo
 			$li_row = $this->io_sql->execute($ls_sql);
 			if($li_row===false)
 			{
-				$this->io_msg->message("CLASE->catalogo MÉTODO->uf_saf_update_catalogo ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
-				$lb_valido=false;
+				$lb_valido[0]=false;
+				$lb_valido[1]=$this->io_msg->message;
 				$this->io_sql->rollback();
 			}
 			else
 			{
-				$lb_valido=true;
-				/////////////////////////////////         SEGURIDAD               /////////////////////////////		
+				$this->io_sql->commit();
+				$lb_valido[0]=true;
+				/////////////////////////////////         SEGURIDAD               /////////////////////////////
 				$ls_evento="UPDATE";
-				$ls_descripcion ="Actualizó ".$as_catalogo." en el Catalogo SIGECOF ";
+				$ls_descripcion ="Actualiz&oacute; ".$as_catalogo." en el Catalogo SIGECOF ";
 				$ls_variable= $this->seguridad->uf_sss_insert_eventos_ventana($aa_seguridad["empresa"],
 												$aa_seguridad["sistema"],$ls_evento,$aa_seguridad["logusr"],
 												$aa_seguridad["ventanas"],$ls_descripcion);
-				/////////////////////////////////         SEGURIDAD               /////////////////////////////		
-				$this->io_sql->commit();
+				if($ls_variable[0] == false)
+				{
+					$lb_valido[1]=$ls_variable[1];
+				}else{
+					$lb_valido[1]="";
+				}
+				/////////////////////////////////         SEGURIDAD               /////////////////////////////
 			}
-		  return $lb_valido;
+		  return json_encode($lb_valido);
 	}// fin uf_saf_update_catalogo
 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//	     Function: uf_saf_delete_catalogo
+	//         Access: public
+	//      Argumento: $as_catalogo     // codigo de catalogo sigecof
+	//                 $aa_seguridad    // arreglo de registro de seguridad
+	//	      Returns: Retorna un Booleano
+	//    Description: Esta funcion elimina un registro al catalogo SIGECOF
+	//	   Creado Por: Ing. Luis Anibal Lang
+	// Fecha Creaciï¿½n: 01/01/2006 								Fecha ï¿½ltima Modificaciï¿½n :
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	function uf_saf_delete_catalogo($as_catalogo,$aa_seguridad)
 	{
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		//	     Function: uf_saf_delete_catalogo
-		//         Access: public 
-		//      Argumento: $as_catalogo     // codigo de catalogo sigecof
-		//                 $aa_seguridad    // arreglo de registro de seguridad
-		//	      Returns: Retorna un Booleano
-		//    Description: Esta funcion elimina un registro al catalogo SIGECOF
-		//	   Creado Por: Ing. Luis Anibal Lang
-		// Fecha Creación: 01/01/2006 								Fecha Última Modificación :  
-		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		$lb_valido=false;
 		$lb_existe=$this->uf_saf_select_activos($as_catalogo);
 		if($lb_existe)
 		{
@@ -174,75 +183,82 @@ class sigesp_saf_c_catalogo
 			else
 			{
 				$ls_sql = " DELETE FROM saf_catalogo".
-						  " WHERE catalogo= '".$as_catalogo. "'"; 
-				$this->io_sql->begin_transaction();	
+						  " WHERE catalogo= '".$as_catalogo. "'";
+				$this->io_sql->begin_transaction();
 				$li_row=$this->io_sql->execute($ls_sql);
 				if($li_row===false)
 				{
-					$this->io_msg->message("CLASE->catalogo MÉTODO->uf_saf_delete_catalogo ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
-					$lb_valido=false;
+					$lb_valido[0]=false;
+					$lb_valido[1]=$this->io_msg->message;
 					$this->io_sql->rollback();
 				}
 				else
 				{
-					$lb_valido=true;
+					$this->io_sql->commit();
+					$lb_valido[0]=true;
 					/////////////////////////////////         SEGURIDAD               /////////////////////////////
 					$ls_evento="DELETE";
-					$ls_descripcion ="Eliminó el Catalogo SIGECOF ".$as_catalogo;
+					$ls_descripcion ="Elimin&oacute; el Catalogo SIGECOF ".$as_catalogo;
 					$ls_variable= $this->seguridad->uf_sss_insert_eventos_ventana($aa_seguridad["empresa"],
 													$aa_seguridad["sistema"],$ls_evento,$aa_seguridad["logusr"],
 													$aa_seguridad["ventanas"],$ls_descripcion);
-					/////////////////////////////////         SEGURIDAD               /////////////////////////////			
-					$this->io_sql->commit();
+					if($ls_variable[0] == false)
+					{
+						$lb_valido[1]=$ls_variable[1];
+					}else{
+						$lb_valido[1]="";
+					}
+					/////////////////////////////////         SEGURIDAD               /////////////////////////////
+
 				}
 			}
 		}
-		return $lb_valido;
+		return json_encode($lb_valido);
 	} //fin  uf_saf_delete_catalogo
 
 	function uf_saf_select_activos($as_catalogo)
 	{
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		//	     Function: uf_saf_select_activos
-		//         Access: public 
+		//         Access: public
 		//      Argumento: $as_catalogo    // codigo de catalogo sigecof
 		//	      Returns: Retorna un Booleano
 		//    Description: Esta funcion verifica si hay activos asociados al renglon del catalogo SIGECOF
 		//	   Creado Por: Ing. Luis Anibal Lang
-		// Fecha Creación: 01/01/2006 								Fecha Última Modificación :  
+		// Fecha Creaciï¿½n: 01/01/2006 								Fecha ï¿½ltima Modificaciï¿½n :
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		$lb_valido=false;
 		$ls_sql = "SELECT * FROM saf_activo  ".
 				  " WHERE codemp='".$this->ls_codemp."'".
 				  " AND catalogo='".$as_catalogo."'" ;
-				 
+
 		$rs_data=$this->io_sql->select($ls_sql);
 		if($rs_data===false)
 		{
-			$this->io_msg->message("CLASE->catalogo MÉTODO->uf_saf_select_activos ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
+			$this->io_msg->message("CLASE->catalogo Mï¿½TODO->uf_saf_select_activos ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
 		}
 		else
 		{
 			if($row=$this->io_sql->fetch_row($rs_data))
 			{
 				$lb_valido=true;
-				
+
 			}
 		}
 		$this->io_sql->free_result($rs_data);
 		return $lb_valido;
 	}//fin uf_saf_select_activos
-	
+
 	function uf_saf_select_articulos($as_catalogo)
 	{
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		//	     Function: uf_saf_select_articulos
-		//         Access: public 
+		//         Access: public
 		//      Argumento: $as_catalogo    // codigo de catalogo sigecof
 		//	      Returns: Retorna un Booleano
 		//    Description: Esta funcion busca una codificacion del catalogo SIGECOF en la tabla de  saf_catalogo
 		//	   Creado Por: Ing. Luis Anibal Lang
-		// Fecha Creación: 01/01/2006 								Fecha Última Modificación :  
+		// Fecha Creaciï¿½n: 01/01/2006 								Fecha ï¿½ltima Modificaciï¿½n :
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		$lb_valido=false;
 		$ls_sql = "SELECT * FROM siv_articulo  ".
@@ -251,14 +267,14 @@ class sigesp_saf_c_catalogo
 		$rs_data=$this->io_sql->select($ls_sql);
 		if($rs_data===false)
 		{
-			$this->io_msg->message("CLASE->catalogo MÉTODO->uf_saf_select_articulos ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
+			$this->io_msg->message("CLASE->catalogo Mï¿½TODO->uf_saf_select_articulos ERROR->".$this->io_funcion->uf_convertirmsg($this->io_sql->message));
 		}
 		else
 		{
 			if($row=$this->io_sql->fetch_row($rs_data))
 			{
 				$lb_valido=true;
-				
+
 			}
 		}
 		$this->io_sql->free_result($rs_data);
